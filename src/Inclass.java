978dd7bf100aa49df6ebf7a0523f10dc730dbf8f
P2_2 <p2_2@US-CompLab07.local>
2017-01-23 13:46:51 -0500

    Initial commit

---
+++ src/Inclass.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+/**
+ * Created by p2_2 on 1/23/17.
+ */
+public class Inclass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String x = scanner.nextLine();
+        System.out.println("You entered"+x);
+
+    }
+}
